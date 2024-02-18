d193e8ef791531097401c23e90e848e1a450f3e3
Yash Goyal <yashgoyalambk@gmail.com>
2024-02-18 18:37:49 +0530

    defined urls and added views and basic templates

--- website/main/views.py
+++ website/main/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
+def home(request):
+    return render(request,"main/index.html")
+
+def contact(request):
+    return render(request,"main/contact.html")
+
+def staff_details(request):
+    return render(request,"main/staff.html")
+
+def downloads(request):
+    return render(request,"main/downloads.html")
