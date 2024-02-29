ea0c2ebd4d2475c66c5259729c612b35fa50596c
Yash Goyal <yashgoyalambk@gmail.com>
2024-02-29 19:16:14 +0530

    added static files

--- website/website/settings.py
+++ website/website/settings.py
@@ -129,3 +129,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 import os
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+STATICFILES_DIRS=[os.path.join(BASE_DIR,"static")]
