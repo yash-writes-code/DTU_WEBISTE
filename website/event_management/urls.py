3cd35f7bdc4b1ea13c952054994d39d96f4becf3
Yash Goyal <86905779+yash-writes-code@users.noreply.github.com>
2024-03-08 15:03:15 +0530

    Delete website directory

--- website/event_management/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import *
-from django.contrib.auth.decorators import login_required
-from .decorators import admin_required
-
-app_name="event_management"
-urlpatterns = [
-    path("dashboard/",admin_required(dashboard),name="dashboard")
-]
